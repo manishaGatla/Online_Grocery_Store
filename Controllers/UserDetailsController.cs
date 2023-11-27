a324fdddcb24e44459a202788b9b2a197881c435
pxk23480 <pxk23480@ucmo.edu>
2023-11-27 11:50:55 -0600

    pushing API

---
+++ Controllers/UserDetailsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineGrocery.Models;
+using OnlineGrocery.services;
+
+namespace OnlineGrocery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailsController : Controller
+    {
+        
+        
+        private readonly MongoConnectionService _mongoConnService;
+
+        public UserDetailsController(MongoConnectionService mongoConnService)
+        {
+            _mongoConnService = mongoConnService;
+        }
+
+        [HttpGet("get")]
+        public String GetUsers(String UserEmail )
+        {
+            try
+            {
+
+                var user = _mongoConnService.GetUserByUseremail(UserEmail);
+                return user.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
