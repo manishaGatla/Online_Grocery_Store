b48dc4d5525011b0a153838be45eaae8d58028c5
pxk23480 <pxk23480@ucmo.edu>
2023-11-28 01:42:46 -0600

    GetCartDetails APi\I

--- Models/CartModel.cs
+++ Models/CartModel.cs
@@ -10,6 +10,19 @@
         public string Category { get; set; }
         public string Product_URL { get; set; }
         public string CustomerEmail { get; set; }
+        public int Quantity { get; set; }
     }
-    
+    public class GetCartModel
+    {
+        public object? _id { get; set; }
+        public string Name { get; set; }
+
+        public string Previous_Price { get; set; }
+        public string Price_Per_Each { get; set; }
+        public string Category { get; set; }
+        public string Product_URL { get; set; }
+        public string CustomerEmail { get; set; }
+        public int Quantity { get; set; }
+    }
+
 }
