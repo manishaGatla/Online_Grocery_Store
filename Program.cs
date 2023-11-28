75829ada4df9c2c0c02098c76f10b07d4edad1d5
pxk23480 <pxk23480@ucmo.edu>
2023-11-27 20:38:42 -0600

    Resolved Cors error

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.Extensions.FileProviders;
 using OnlineGrocery.services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,29 +13,45 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("CorsPolicy",
-//        builder =>
-//        {
-//            builder.WithOrigins(["*"])
-//                   .WithHeaders(corsSettings.AllowedHeaders.ToArray())
-//                   .WithMethods(corsSettings.AllowedMethods.ToArray());
-//        });
-//});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:4200") // Update with your Angular app's URL
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<MongoConnectionService>();
 var app = builder.Build();
 
+
+app.UseCors("AllowOrigin");
+//app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "FED")),
+    RequestPath = ""
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
-
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+    {
+        context.Request.Path = "/index.html";
+        await next();
+    }
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
