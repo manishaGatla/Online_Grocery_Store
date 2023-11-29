30c0df444190277ad97c35736d9889a72d0c8db3
Kiran Kumar Kongari <kongarikiran77@gmail.com>
2023-11-28 23:07:04 -0600

    front end changes

--- FED/src/app/components/payments/payments.component.ts
+++ FED/src/app/components/payments/payments.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { LoginService } from 'src/app/services/login.service';
 import { PaymentsService } from 'src/app/services/payments.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payments',
@@ -10,7 +11,7 @@ import { PaymentsService } from 'src/app/services/payments.service';
 })
 export class PaymentsComponent implements OnInit {
 
-  constructor(public cartService: CartService, private paymentService: PaymentsService, private loginService: LoginService){}
+  constructor(public cartService: CartService, private paymentService: PaymentsService, private loginService: LoginService, private router : Router){}
   @Input() cartItems: any ;
   cardNumber: any;
   cardHolderName: any;
@@ -41,21 +42,34 @@ export class PaymentsComponent implements OnInit {
   }
 
   onSubmit(){
+    var total = 0;
+    this.cartService.cartItems.forEach((p: any)=>{
+      total = total + p.total;
+    })
     var reqBody = {
-    accountNumber         : this.accountNumber          ,
-    cvv                   : this.cvv                   ,
-    selectedPaymentMethod : this.selectedPaymentMethod ,
-    billingAddress        : this.billingAddress        ,
-    cardHolderName        : this.cardHolderName        ,
-    cartItems: this.cartItems,
-    customerId: this.loginService.profileDetails._id,
-    orderDate: new Date(),
-    address: this.cartService.addressDetails   
-
+  
+      orderDetails :{
+        deliveryAddress :this.cartService.selectedDeliveryOption == 'Store Pickup' ? null:  JSON.stringify(this.cartService.addressDetails) ,
+        customerId : this.loginService.profileDetails._id,
+        orderStatus:"Order Confirmed",
+        deliveryType: this.cartService.selectedDeliveryOption
+      },
+      cartDetails: this.cartService.cartItems,
+      paymentDetails:{
+        cardNumber         : this.accountNumber          ,
+        cvv                   : this.cvv                   ,
+        paymentMethod : this.selectedPaymentMethod ,
+        billingAddress        : this.billingAddress        ,
+        cardHolderName        : this.cardHolderName        ,
+        customerName: this.loginService.profileDetails.name,
+        customerId: this.loginService.profileDetails._id,
+        orderId: null,
+        amount: "$" + total
+      }
     }
 
-    this.paymentService.placeOrder(reqBody).subscribe((res) =>{
-      
+    this.paymentService.placeOrder(reqBody, this.loginService.profileDetails.email).subscribe((res) =>{
+      this.router.navigateByUrl('/orders');
     })
   }
 
@@ -63,3 +77,4 @@ export class PaymentsComponent implements OnInit {
     this.cartService.showPaymentSection = false;
   }
 }
+
